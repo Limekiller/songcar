a12e6bb6df8845e6bb99c873da524f38d799787d
Limekiller <me@bryceyoder.com>
2025-11-26 16:35:10 -0500

    Add URL to metadata

--- beacon.sh
+++ beacon.sh
@@ -4,8 +4,9 @@ while true; do
     ARTIST="$(playerctl metadata --format '{{ artist }}')"
     SONG="$(playerctl metadata --format '{{ title }}')"
     ALBUM="$(playerctl metadata --format '{{ album }}')"
+    URL="$(playerctl metadata --format '{{ xesam:url }}')"
 
-    JSON=$(jq -n --arg 'song' "$SONG" '$ARGS.named' --arg 'artist' "$ARTIST" '$ARGS.named' --arg 'album' "$ALBUM" '$ARGS.named')
+    JSON=$(jq -n --arg 'song' "$SONG" '$ARGS.named' --arg 'artist' "$ARTIST" '$ARGS.named' --arg 'album' "$ALBUM" '$ARGS.named' --arg 'url' "$URL" '$ARGS.named')
 
     #curl --location --request POST 'http://localhost:3000/update' \
     curl --location --request POST 'http://192.168.0.124:3000/update' \
